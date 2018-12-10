7d87fe09f73f4db931eda33d1f172f36b0de777e
tejakathari007 <tejakathari007@gmail.com>
2018-12-10 15:07:39 +0530

    added poc

---
+++ Poc/Spring-Kafka-Consumer/src/main/java/com/charter/listener/KafkaConsumer.java
@@ -0,0 +1,22 @@
+package com.charter.listener;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import com.charter.model.User;
+
+@Service
+public class KafkaConsumer {
+
+    @KafkaListener(topics = "Kafka_Example", groupId = "group_id")
+    public void consume(String message) {
+        System.out.println("Consumed message: " + message);
+    }
+
+
+    @KafkaListener(topics = "Kafka_Example_json", groupId = "group_json",
+            containerFactory = "userKafkaListenerFactory")
+    public void consumeJson(User user) {
+        System.out.println("Consumed JSON Message: " + user);
+    }
+}
---
+++ Readme.txt
@@ -0,0 +1,12 @@
+
+
+Start Zookeeper
+from C:\kafka_2.12-2.1.0\kafka_2.12-2.1.0
+.\bin\windows\zookeeper-server-start.bat .\config\zookeeper.properties
+
+Start Kafka in windows 
+from C:\kafka_2.12-2.1.0\kafka_2.12-2.1.0
+.\bin\windows\kafka-server-start.bat .\config\server.properties
+
+
+Ref: https://dzone.com/articles/running-apache-kafka-on-windows-os
